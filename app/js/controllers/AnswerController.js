91a4727291d12ccd7c42922b65bd57e5c81ba1bc
satonin18 <satonin18@mail.ru>
2019-08-27 18:38:30 +0500

    Отправка данных
    https://metanit.com/web/angular/3.8.php

--- app/js/controllers/AnswerController.js
+++ app/js/controllers/AnswerController.js
@@ -1,9 +1,15 @@
-questApp.controller('AnswerController', 
-    function AnswerController($scope){
+questApp.controller('AnswerController',
+    function AnswerController($scope, $http){
+         
+        $scope.response={};
         $scope.save = function (answer, answerForm){
             if(answerForm.$valid){
-                // действия по сохранению
-                alert(answer.author + ", ваш ответ сохранен");
+                 
+                $http.post("postAnswer.php", answer)
+                    .then(function success (response) {
+                        $scope.response = response.data;
+                    });
             }
         };
-})
+    }
+)
