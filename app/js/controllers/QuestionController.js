66b07ed857caf0ee4925d1a607a690bf96cae818
satonin18 <satonin18@mail.ru>
2019-08-27 13:46:40 +0500

    Cоздание собственных фильтров
    https://metanit.com/web/angular/2.5.php

--- app/js/controllers/QuestionController.js
+++ app/js/controllers/QuestionController.js
@@ -1,39 +1,35 @@
-var myApp=angular.module('myApp');
-myApp.controller('QuestionController', function($scope) {
-    
-	$scope.sortparam='-rate';
-    $scope.question={
-        text: 'Какой js-фреймворк лучше использовать?',
-        author: 'Иван Иванов',
-        date: '20/10/2013',
-        answers: 
-        [{
-            text: 'AngularJS!',
-            author: 'Вова Сидоров',
-            date: '20/10/2013',
-            rate:2
-        },{
-            text: 'AngularJS лучше всех',
-            author: 'Олег Кузнецов',
-            date: '20/10/2013',
-            rate:0
-        },{
-            text: 'Я бы использовал knockout',
-            author: 'Неизвестный',
-            date: '21/10/2013',
-            rate:0
-        }]
-    };
+questApp.controller('QuestionController', 
+    function QuestionController($scope){
      
-    $scope.voteUp = function (answer){
-        answer.rate++;
-    };
-    $scope.voteDown = function (answer){
-        answer.rate--;
-    };
-    $scope.questColorClass= "questcolor";
-    $scope.changeClass = function (e) {
-         
-        $scope.questColorClass = e.type == "mouseover" ? "questselectedcolor" : "questcolor";
+        $scope.question={
+            text: 'Какой js-фреймворк лучше использовать?',
+            author: 'Иван Иванов',
+            date: '20/10/2013',
+            answers: 
+            [{
+                text: 'AngularJS!',
+                author: 'Вова Сидоров',
+                date: '20/10/2013',
+                rate:2
+            },{
+                text: 'AngularJS лучше всех',
+                author: 'Олег Кузнецов',
+                date: '21/10/2013',
+                rate:3
+            },{
+                text: 'фигасе квестшен',
+                author: 'Неизвестный',
+                date: '22/10/2013',
+                rate:-1
+            }]
+        },
+     
+        $scope.voteUp = function (answer){
+            answer.rate++;
+        };
+        $scope.voteDown = function (answer){
+            answer.rate--;
+        };
+
     }
-});
+)
