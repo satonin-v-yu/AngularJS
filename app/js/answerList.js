5b55bddd9f7fd4a5c918a910f403e3dac71ba84a
satonin18 <satonin18@mail.ru>
2019-08-27 15:38:00 +0500

    Внедрение шаблонов  (опечатка в коде с filterFormatText)
    https://metanit.com/web/angular/2.16.php
    +need sever (by url)

--- app/js/answerList.js
+++ app/js/answerList.js
@@ -4,19 +4,21 @@ questApp.directive("answerList", function () {
             scope.data = scope[attrs["answerList"]];
         },
         restrict: "A",
-		//templateUrl: "templates/answersTemplate.html"
-		
+		//templateUrl: "templates/answersTemplate.html",
+		transclude: true,
+		/*
         template: "<div class='answers'>" +
+		//"<div ng-transclude></div>" +
         "<div ng-repeat='answer in data.answers' class='answer'>" +
             "<div class='vote'>" +
             "<a class='vote-up' ng-click='voteUp(answer)'></a>" +
             "<span class='rate'>{{answer.rate}}</span>" +
             "<a class='vote-down' ng-click='voteDown(answer)'></a>" +
         "</div>" +
-        "<b>{{answer.text}}</b>" +
+        "<b>{{answer.text | formatText}}</b>" +
         "<p>{{answer.author}}</p>" +
         "<p><i>{{answer.date}}</i></p>" +
 		"</div></div>"
-		
+		*/
     }
 });
