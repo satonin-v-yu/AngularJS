3b3576c5de282160bde385d8810727f4d6488cb5
satonin18 <satonin18@mail.ru>
2019-08-27 10:46:24 +0500

    Обход внутренних массивов

--- app/js/controllers/phonesController.js
+++ app/js/controllers/phonesController.js
@@ -1,29 +1,23 @@
 var myApp=angular.module('myApp');
 myApp.controller('phoneController', function($scope) {
- 
-  $scope.phones = [{
-        name: 'Nokia Lumia 630',
-        year: 2014,
-        price: 200,
-        company: {
-            name: 'Nokia',
-            country: 'Финляндия'
-        }
+	
+	$scope.phones = [{
+        name: 'Nokia',
+        models:[{
+            name: "Nokia Lumia 530",
+            price: 150
+        },{
+            name: "Nokia X2",
+            price: 200
+        }]
     },{
-        name: 'Samsung Galaxy S 4',
-        year: 2014,
-        price: 400,
-        company: {
-            name: 'Samsung',
-            country: 'Республика Корея'
-        }
-    },{
-        name: 'Mi 5',
-        year: 2015,
-        price: 300,
-        company: {
-            name: 'Xiaomi',
-            country: 'Китай'
-        }
+        name: 'Samsung',
+        models:[{
+            name: "Samsung Galaxy S5",
+            price: 500
+        },{
+            name: "Samsung Galaxy Alpha",
+            price: 400
+        }]
     }]
 });
