28f00580397c916114b5613afa725795f33aae7e
satonin18 <satonin18@mail.ru>
2019-08-27 15:14:40 +0500

    Шаблоны директив (строчный шаблон)
    https://metanit.com/web/angular/2.15.php

--- app/js/answerList.js
+++ app/js/answerList.js
@@ -1,38 +1,19 @@
 questApp.directive("answerList", function () {
     return {
         link: function (scope, element, attrs) {
-         
-            var data = scope[attrs["answerList"] || attrs["source"]];
-             
-            if (angular.isArray(data.answers)) {
-                var divElem = angular.element("<div>").addClass("answers");
-                if (element[0].nodeName == "#comment") {
-                    element.parent().append(divElem);
-                } 
-                else {
-                    element.append(divElem);
-                }
-                for (var i = 0; i < data.answers.length; i++) {
-                    var answerElem = angular.element('<div>').addClass("answer");
-                    var voteElem = angular.element('<div>').addClass("vote");
-                     
-                    var voteUpElem = angular.element('<a>').addClass("vote-up");
-                    voteElem.append(voteUpElem);
-                    var rateElem = angular.element('<span>').addClass("rate").text(data.answers[i].rate);
-                    voteElem.append(rateElem);
-                    var voteDownElem = angular.element('<a>').addClass("vote-down");
-                    voteElem.append(voteDownElem);
-                    answerElem.append(voteElem);
-                     
-                    answerElem.append(angular.element('<b>').text(data.answers[i].text));
-                    answerElem.append(angular.element('<p>').text(data.answers[i].author));
-                    var iElem =angular.element('<i>').text(data.answers[i].date);
-                    var dateElem = angular.element('<p>').append(iElem);
-                    answerElem.append(dateElem);
-                    divElem.append(answerElem);
-                }
-            }
+            scope.data = scope[attrs["answerList"]];
         },
-        restrict: "EACM"
+        restrict: "A",
+        template: "<div class='answers'>" +
+        "<div ng-repeat='answer in data.answers' class='answer'>" +
+            "<div class='vote'>" +
+            "<a class='vote-up' ng-click='voteUp(answer)'></a>" +
+            "<span class='rate'>{{answer.rate}}</span>" +
+            "<a class='vote-down' ng-click='voteDown(answer)'></a>" +
+        "</div>" +
+        "<b>{{answer.text}}</b>" +
+        "<p>{{answer.author}}</p>" +
+        "<p><i>{{answer.date}}</i></p>" +
+    "</div></div>"
     }
 });
