2d344f2c71831c070f0d4901c184f187285a002f
satonin18 <satonin18@mail.ru>
2019-08-28 12:30:19 +0500

    Управление маршрутом и отключение кэширования
    https://metanit.com/web/angular/4.2.php

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,15 @@
+/*
+$route.
+current: возвращает объект, который содержит информацию о текущем маршруте
+reload(): перезагружает представление для текущего маршрута
+routes: возвращает коллекцию маршрутов, определенных через $routeProvider
+
+    мы можем обрабатывать события, определенные в сервисе $route:
+$routeChangeStart: событие срабатывает перед изменением маршрута
+$routeChangeSuccess: событие срабатывает после изменения маршрута
+$routeUpdate: событие срабатывает при обновлении маршрута
+$routeChangeError: срабатывает, если маршрут не может быть изменен
+*/
 var questApp = angular.module('questApp', ["ngRoute"])
     .config(function($routeProvider){
         $routeProvider.when('/question',
@@ -11,4 +23,11 @@ var questApp = angular.module('questApp', ["ngRoute"])
             controller:'AnswerController'
         });
         $routeProvider.otherwise({redirectTo: '/question'});
-});
+
+}).run(function($rootScope, $templateCache) {
+    $rootScope.$on('$routeChangeStart', function(event, next, current) {
+        if (typeof(current) !== 'undefined'){
+            $templateCache.remove(current.templateUrl);
+        }
+    });
+}); 
